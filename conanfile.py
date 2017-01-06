19223b6572f7c398cf3e5ecd6738b78b3aa231cf
pjohalloran <pjohalloran@gmail.com>
2017-01-06 21:05:20 +0000

    Adding C++ 11 14 options

--- conanfile.py
+++ conanfile.py
@@ -7,6 +7,8 @@ class FilesystemConan(ConanFile):
   license="Modified BSD License (3-Clause BSD license)"
   settings = "os", "compiler", "build_type", "arch"
   url = "https://github.com/pjohalloran/conan-filesystem"
+  options = {"compiler_version": ["11", "14"]}
+  default_options = "compiler_version=14",
 
   def source(self):
     self.run("git clone https://github.com/wjakob/filesystem")
@@ -15,6 +17,6 @@ class FilesystemConan(ConanFile):
     self.copy("*.h", src="filesystem/filesystem", dst="include", keep_path=True)
 
   def package_info(self):
-    self.cpp_info.sharedlinkflags = ["-std=c++11"]
-    self.cpp_info.exelinkflags = ["-std=c++11"]
-    self.cpp_info.cppflags = ["-std=c++11", "-stdlib=libc++"]
+    self.cpp_info.sharedlinkflags = ["-std=c++%s" % self.options.compiler_version]
+    self.cpp_info.exelinkflags = ["-std=c++%s" % self.options.compiler_version]
+    self.cpp_info.cppflags = ["-std=c++%s" % self.options.compiler_version, "-stdlib=libc++"]
